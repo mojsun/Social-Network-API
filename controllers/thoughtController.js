835828c921d451faea6026941cb8912ece087f75
mojsun <m.khorashahi7@gmail.com>
2023-03-14 21:52:56 -0400

    modifying thought controlllers and thought routes to add reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -73,4 +73,31 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  // Create a reaction stored in a single thought's reactions array field
+  createReaction: (req, res) => {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $push: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((Thought) => {
+        if (!Thought) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(Thought);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // Delete a reaction by the reaction's reactionId value
+  deleteReaction: (req, res) => {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { new: true }
+    )
+      .then((Thought) => res.json(Thought))
+      .catch((err) => res.json(err));
+  },
 };
